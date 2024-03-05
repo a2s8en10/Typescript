cd552888263e981cc03443e3d7e034ff23e347d6
Anurag Sahu <anu.sahu9589@gmail.com>
2024-03-05 18:19:56 +0530

    Add code
    function type

---
+++ function.ts
@@ -0,0 +1,47 @@
+function add ()
+{
+    return 100 ;
+}
+// function type is number because numeric return
+
+function num ()
+{
+    return 'asd';
+}
+// function type is string because string return
+
+// * function type define
+
+function val() : number // return type is only numeric because function type are define
+{
+    return 12;
+}
+
+function sub() : string // return type is only numeric because function type are define
+{
+    return 'anu';
+}
+
+// only required the function are number and string
+
+function str() : number | string
+{
+    return 23;
+}
+
+function mux (a:number,b:number) : number
+{
+    return a+b ;
+}
+
+console.log(mux(12,8));
+
+// Let pass the variable
+
+const a = 12;
+const b = 'add';
+
+function check () : number
+{
+    return a;
+}
