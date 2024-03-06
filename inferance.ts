eca4add63e3c708a30e6e2578edd893dd1e8f42a
Anurag Sahu <anu.sahu9589@gmail.com>
2024-03-06 20:49:36 +0530

    Add code

--- inferance.ts
+++ inferance.ts
@@ -1,15 +1,14 @@
-
-// use the let 
+// use the let
 
 let data = 1231; // data type is number because data value is fixed
 // data = 12;
-// data = 'eteesg'; this line is show the error because change the type string only number type 
+// data = 'eteesg'; this line is show the error because change the type string only number type
 
 let val = 'xyz'; // data type is string because value is fixed
-// data = 'eteesg'; 
+// data = 'eteesg';
 // data = 12; this line is show the error because change the type number type only string
 
-let num ; // this data type is any becasuse not a given value 
+let num ; // this data type is any becasuse not a given value
 
 // use all type
 
@@ -20,9 +19,9 @@ num = 'xyz';
 num = true;
 
 
-// use the const 
+// use the const
 
-const num2 = 10; // 10 is the type and fixed the value not change 
+const num2 = 10; // 10 is the type and fixed the value not change
 
 const str = 'xyz';
 
