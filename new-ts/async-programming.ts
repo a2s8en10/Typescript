2d69dcbe6baa0eb4465615af85c44db9bc04e715
Anurag Sahu <anu.sahu9589@gmail.com>
2025-05-27 17:59:03 +0530

    Update Code

--- new-ts/async-programming.ts
+++ new-ts/async-programming.ts
@@ -41,6 +41,14 @@ const fetchData = (value: boolean): Promise<String> => {
   });
 };
 
+fetchNewData(true)
+  .then((data) => {
+    console.log("Data : ", data);
+  })
+  .catch((error) => {
+    console.log("Error : ", error);
+  });
+
 // Chaining promise
 // fetchData(true)
 //   .then((data) => {
@@ -54,19 +62,104 @@ const fetchData = (value: boolean): Promise<String> => {
 //     console.log("Error:", error);
 //   });
 
-// nested promise
-fetchData(true)
-  .then((data) => {
-    console.log("Success:", data);
+// nested promise--------------
+// console.log("nested promise--------------");
 
-    fetchData(false)
-      .then((data) => {
-        console.log("Success:", data);
-      })
-      .catch((error1) => {
-        console.log("Error : ", error1);
-      });
+// fetchData(true)
+//   .then((data) => {
+//     console.log("Success:", data);
+
+//     fetchData(false)
+//       .then((data) => {
+//         console.log("Success:", data);
+//       })
+//       .catch((error1) => {
+//         console.log("Error : ", error1);
+//       });
+//   })
+//   .catch((error) => {
+//     console.log("Error:", error);
+//   });
+
+// async await ------------------------
+console.log("async await ------------------------");
+
+async function fetchNewData(value: boolean): Promise<string> {
+  return "Data are Fetch";
+}
+
+// Single Time use the Promise
+fetchNewData(true)
+  .then((data) => {
+    console.log("Data : ", data);
   })
   .catch((error) => {
-    console.log("Error:", error);
+    console.log("Error : ", error);
+  });
+
+// multiple time use the Promise
+async function getFetchNewData(): Promise<void> {
+  try {
+    const data = await fetchNewData(true);
+    const data1 = await fetchNewData(false);
+  } catch (error) {
+    console.log("Error : ", error);
+  }
+}
+
+// Working With api using fetch
+
+interface Post {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: string;
+}
+
+async function fetchPosts(): Promise<Post[]> {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    // json data
+    const posts: Post[] = await response.json();
+    return posts;
+  } catch (error: any) {
+    console.log("Error : ", error);
+    throw new Error(error);
+  }
+}
+
+fetchPosts()
+  .then((data) => {
+    console.log("Post : ", data);
+  })
+  .catch((error) => {
+    console.log("error : ", error);
+  });
+
+// concurrent promises with promise.all
+async function taskPro1() {
+  console.log("task 1...");
+  return "task Promise 1";
+}
+async function taskPro2() {
+  console.log("task 2...");
+  return "task Promise 2";
+}
+
+async function fetchPro(): Promise<[string, string]> {
+  const [pro1, pro2] = await Promise.all([
+    taskPro1(),
+    taskPro2()
+  ]);
+
+  return [pro1, pro2];
+}
+
+fetchPro()
+  .then((data) => {
+    console.log("data : ", data);
+  })
+  .catch((error) => {
+    console.log("error : ", error);
   });
